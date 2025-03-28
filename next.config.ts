9070ab8f992e2b250fecd0027e2f5de55c4f3ebc
Yosef Hershberg <berioo2000@gmail.com>
2025-03-28 12:52:31 +0300

    fixed prisma build for prod

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('_http_common');
+    }
+    return config;
+  },
+  // Optional: If using API routes with Prisma
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
   images: {
     domains: ['lh3.googleusercontent.com'], // Add the hostname here
   },
